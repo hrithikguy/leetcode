17d3d2a685131fc7d2af747d14d33a1cf611e526
Aditya Gudibanda <adityagudibanda1995@gmail.com>
2019-01-13 13:03:22 -0500

    Create 905_sort_array_by_parity.py

---
+++ 905_sort_array_by_parity.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        
+        
+        B = sorted(A, key = lambda x: x % 2)
+        return B
+        
