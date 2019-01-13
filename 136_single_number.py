35f49b98b86e530c179ae0407614de82ae0d4d01
Aditya Gudibanda <adityagudibanda1995@gmail.com>
2019-01-13 13:00:09 -0500

    Create 136_single_number.py

---
+++ 136_single_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        output = 0
+        
+        for i in nums:
+            output = output ^ i
+            
+        return output
